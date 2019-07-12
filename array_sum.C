6dde0ff48ee13ea24a031cadd34c12b5348a2cd9
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-12 11:03:58 +0200

    Update array_sum.C

--- array_sum.C
+++ array_sum.C
@@ -6,9 +6,7 @@ int sum_of_array(int *arr_input, int arr_len) {
   int sum_arr = 0;
   
   for(int  i =0; i<arr_len; i++){
-    printf("\n result in d %d", arr_input[i]);
     sum_arr +=arr_input[i];
-      
   }
   
   return sum_arr;
@@ -21,12 +19,14 @@ int main(void) {
  // int length = sizeof(input)/sizeof(input[0]);
   printf("Please  enter  your array length\n");
   scanf("%d", &length);
+  
   printf("Please  enter  your array\n");
   for (x=0; x<length;x++)
   {
       printf("Enter number %d \n", (x+1));
       scanf("%d", &input[x]);
   }
+  
   printf("the array entered with length %d is\n", length);
   for( i=0; i<length; i++){
     printf("%d\n", input[i]);
