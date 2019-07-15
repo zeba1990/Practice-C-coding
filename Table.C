70133b47ccacd202c4845193c15484f16a58403e
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-15 17:41:44 +0200

    Add files via upload

---
+++ Table.C
@@ -0,0 +1,14 @@
+﻿//print table of any number entered
+#include <stdio.h>
+
+int main(void) {
+  int num;
+
+  printf("Please enter the number \n");
+  scanf("%d\n", &num);
+  
+  for(int i=1; i<=10; i++) {
+    printf("%d * %d = %d\n",num,i, num*i);
+  }
+  return 0;
+}
