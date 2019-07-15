4382392fbcab804a441fae00740e04efff0792c5
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-15 15:56:07 +0200

    Add files via upload

---
+++ Pattern.C
@@ -0,0 +1,14 @@
+﻿//print pattern
+
+#include <stdio.h>
+
+int main(void) {
+  
+  for(int i = 1; i< 6; i++){
+    for(int j =0; j<i; j++){
+        printf("*");
+    }
+     printf("\n");
+  }
+  return 0;
+}
---
+++ Pattern_join.C
@@ -0,0 +1,20 @@
+﻿//print joint pattern
+
+#include <stdio.h>
+
+int main(void) {
+  
+  for(int i = 0; i< 7; i++){
+    for(int j =0; j<i; j++){
+        printf("*");
+    }
+     printf("\n");
+  }
+  for(int i = 5; i> 0; i--){
+    for(int j =i; j>0; j--){
+        printf("*");
+    }
+     printf("\n");
+  }
+  return 0;
+}
