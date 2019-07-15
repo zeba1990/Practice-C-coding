2dac74657bf6ea139cc7fee173042c211fa98af2
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-15 18:24:00 +0200

    Add files via upload

---
+++ Factorial.C
@@ -0,0 +1,17 @@
+﻿//factorial of a number
+
+#include <stdio.h>
+
+int main(void) {
+  int num, fact = 1;
+
+  printf("Please enter the number \n");
+  scanf("%d\n", &num);
+
+  for(int i= num; i>=1; i--){
+    fact *= i;
+  }
+  
+  printf("%d ", fact);
+  return 0;
+}
