663cfad1ae2aa31a844f1f4f84a92e31fc1986b1
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-16 18:03:53 +0200

    Add files via upload

---
+++ Pattern_join_leftside.C
@@ -0,0 +1,32 @@
+﻿//pattern join leftside
+
+#include <stdio.h>
+
+int main(void) {
+  
+ for(int i=6;i>0;i--){
+    for(int j=1;j<=6;j++){
+     
+      if(j>i){
+        printf("*");
+      } else{
+        printf(" ");
+      } 
+    }
+    printf("\n");
+  }
+
+  for(int i=0;i<=6;i++){
+    for(int j=1;j<=6;j++){
+     
+      if(j>i){
+        printf("*");
+      } else{
+        printf(" ");
+      } 
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
