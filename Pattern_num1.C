98b626a6255337bcf6cc6876d4c5570841d2dd22
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-16 18:18:56 +0200

    Add files via upload

---
+++ Pattern_num1.C
@@ -0,0 +1,18 @@
+﻿//Pattern with num
+
+#include <stdio.h>
+
+int main(void) {
+  
+ for(int i=0;i<6;i++){
+    for(int j=1;j<=6;j++){
+      if(j<=i){
+        printf("%d ", j);
+      }
+      
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
---
+++ Pattern_num2.C
@@ -0,0 +1,22 @@
+﻿//Pattern with nums
+
+#include <stdio.h>
+
+int main(void) {
+  
+ int k =1;
+
+ for(int i=0;i<6;i++){
+    for(int j=1;j<=6;j++){
+      if(j<=i){
+        printf("%d ", k);
+        k=k+1;
+      }
+      
+    }
+    
+    printf("\n");
+  }
+
+  return 0;
+}
