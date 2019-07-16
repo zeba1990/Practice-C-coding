fa4e7760d51bec6bc0124054abdf3844a577f0ae
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-16 15:32:51 +0200

    Add files via upload

---
+++ fibonacci.C
@@ -0,0 +1,21 @@
+﻿//Fibonacci series
+
+
+#include <stdio.h>
+
+int main(void) {
+  int num=0 , temp =1, nexterm=0;
+  int n;
+  printf("enter the number of terms of fibonacci series  ");
+  scanf("%d", &n);
+
+  printf("%d   ",num);
+  
+  for(int i =1; i<n; i++){
+    nexterm=num +temp;
+    num=temp ;
+    temp=nexterm ;
+    printf("%d   ",temp);
+  }
+  return 0;
+}
